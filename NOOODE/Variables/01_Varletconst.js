6add6302cb69e8c0870ed41f1f0aa807e94b0a0c
Rohit-K-V <121821289+Rohit-K-V@users.noreply.github.com>
2024-02-27 11:47:44 +0530

    Add files via upload

---
+++ NOOODE/Variables/01_Varletconst.js
@@ -0,0 +1,10 @@
+const accountID=144
+let accountEmail="rohith1654@gmail.com"
+var password="3333"
+console.log(accountEmail)
+console.table([accountID,accountEmail,password])
+//to declare variables there are two ways that is using var and let
+//var has some issues in block scope and functional scope,Variables created by var are available throughout the functional scope and 
+// declared using let is only available in the block scope which increases the security of the variables
+let acstate
+consloe.log(acstate)
